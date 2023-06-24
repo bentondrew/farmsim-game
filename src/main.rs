5fdc17d2a620d15479db82aa852f8c2ca60b0aae
Benton Drew <bentondrew24@gmail.com>
2023-06-24 16:21:24 -0500

    Rework player and associated camera (#12)
    * Spawns the player's camera as a child entity of the player entity. This makes the camera transform relative to the player entity so when the player moves, the camera location is automatically updated.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-mod cameras;
 mod characters;
 mod game_world;
 mod lighting;
@@ -9,8 +8,7 @@ use bevy::{
     prelude::{default, DefaultPlugins, PluginGroup, Vec3},
     window::{Window, WindowPlugin},
 };
-use cameras::generate_add_camera_system;
-use characters::players::generate_add_player_system;
+use characters::player::entity::generate_add_player_system;
 use game_world::add_ground_plane;
 use lighting::add_light;
 use player_control::{gamepad_connection_events, generate_move_player_system};
@@ -42,7 +40,6 @@ fn main() {
         .add_startup_system(generate_add_player_system(player.id, player.spawn_location))
         .add_startup_system(add_ground_plane)
         .add_startup_system(add_light)
-        .add_startup_system(generate_add_camera_system(player.id, player.spawn_location))
         .add_system(gamepad_connection_events)
         .add_system(generate_move_player_system(player.id))
         .run();
